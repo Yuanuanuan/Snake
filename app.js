3f426b6e3f311ec9e298adaf6d36462bb623ad94
Yuanuanuan <asdll6618@gmail.com>
2023-10-17 18:00:14 +0800

    feat: snake move

--- app.js
+++ app.js
@@ -12,38 +12,60 @@ let snake = []; // array中的每個元素，都是一個物件
 //  物件的工作是，儲存身體的x, y座標
 
 snake[0] = {
-  X: 80,
-  y: 0,
+  x: 80,
+  y: 0
 }
 
 snake[1] = {
-  X: 60,
-  y: 0,
+  x: 60,
+  y: 0
 }
 
 snake[2] = {
-  X: 40,
-  y: 0,
+  x: 40,
+  y: 0
 }
 
 snake[3] = {
-  X: 20,
-  y: 0,
+  x: 20,
+  y: 0
 }
+
+const changeDirection = (e) => {
+  if (e.keyCode === 37 && d != 'right') {
+    // left
+    d = 'left'
+  } else if (e.keyCode === 38 && d != 'down') {
+    // Up
+    d = 'up'
+  } else if (e.keyCode === 39 && d != 'left') {
+    // right
+    d = 'right'
+  } else if (e.keyCode === 40 && d != 'up') {
+    // down
+    d = 'down'
+  }
+}
+
+window.addEventListener('keydown', changeDirection)
 
 let d = 'right';
 
 const draw = () => {
   console.log('hello')
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
   for (let i = 0; i < snake.length; i++) {
-    if (i === 0) {
+    if (i == 0) {
       ctx.fillStyle = '#d2a9a3';
     } else {
       ctx.fillStyle = '#cfe6ed';
     }
     ctx.strokeStyle = 'white';
-    ctx.fillRect(snake[i].X, snake[i].y, unit, unit);
-    ctx.strokeRect(snake[i].X, snake[i].y, unit, unit);
+
+    ctx.fillRect(snake[i].x, snake[i].y, unit, unit);
+    ctx.strokeRect(snake[i].x, snake[i].y, unit, unit);
   }
 
   let snakeX = snake[0].x;
@@ -60,7 +82,7 @@ const draw = () => {
 
   let newHead = {
     x: snakeX,
-    y: snakeY,
+    y: snakeY
   }
 
   snake.pop();
